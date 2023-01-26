a0b4ee390a117c9b1fdabb3571156cfb3013739c
Juan Arnau <juanarnau81@gmail.com>
2023-01-26 17:11:22 -0300

    cambio deploy back

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './redux/store';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://piback-production-c83a.up.railway.app';
+axios.defaults.baseURL = 'https://piback-production-b28c.up.railway.app';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
