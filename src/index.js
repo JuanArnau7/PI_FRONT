2a6bdcc4a42be7d3300d0ffa180f2e80b9621cb8
Juan Arnau <juanarnau81@gmail.com>
2023-02-27 16:28:01 -0300

    cambio ruta back deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001';
 // axios.defaults.baseURL = 'https://piback-production-b28c.up.railway.app';
-
 axios.defaults.baseURL = 'https://piback-production-aff5.up.railway.app';
 ReactDOM.render(
   <Provider store={store}>
